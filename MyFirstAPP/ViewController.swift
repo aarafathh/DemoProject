ca40ddf9e3ce1fff12f4835d397b6a548bd2a6c2
Arafath Hossain <aarafathh@gmail.com>
2024-10-22 22:25:19 +0600

    First commit

---
+++ MyFirstAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstAPP
+//
+//  Created by Arafath Hossain on 19/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
